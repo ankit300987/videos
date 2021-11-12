0fdee5ee4d0fec3efb01190e87dd823a65107302
Ankit Srivastava <ankitsrivastava.fsd@gmail.com>
2021-11-12 22:03:55 +0530

    Updated Seach Component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,30 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { val: "Search Here!!" };
 
-    render() {
-        return (
-            <div>
-                Seach Bar Component Loaded
-            </div>
-        );
-    }
+  onChangeEvent = (e) => {
+    this.setState({
+      val: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <div className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.val}
+              onChange={this.onChangeEvent}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
