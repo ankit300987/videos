3de4a775017ecba412bdd790854df674bd299164
Ankit Srivastava <ankitsrivastava.fsd@gmail.com>
2021-11-13 11:10:11 +0530

    Added Video list component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../API/youtube";
+import VideoList from "./VideoList";
+
 
 class App extends React.Component {
-  onSearchInitated = val =>{
-    youtube.get('/search',{
+  state = {video:[]};
+
+  onSearchInitated = async val =>{
+    const response = await youtube.get('/search',{
       params:{
         q: val
       }
     });
+    this.setState({
+      video: response.data.items
+    });
+    console.log(this.state.video);
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSearchSubmission={this.onSearchInitated}/>
+        <VideoList videos={this.state.video}/>
       </div>
     );
   }
